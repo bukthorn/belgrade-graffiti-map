eb12949130778cca325ed85077e1e23730134794
bukthorn <unthouckseen@gmail.com>
2026-05-07 00:31:15 +0200

    Create graffiti.ts

---
+++ src/types/graffiti.ts
@@ -0,0 +1,9 @@
+export type GraffitiItem = {
+  latitude?: string
+  longitude?: string
+  original_text?: string
+  possible_meaning?: string
+  tags?: string
+  date?: string
+  [key: string]: string | undefined
+}
